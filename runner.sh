2e7340e5cdc1a8f0518a02c14456fa115e4ae6e4
Freedom Force Ukraine <1054289+Biogent@users.noreply.github.com>
2022-06-16 22:44:56 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -105,10 +105,10 @@ then
 		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$copies_num_sametargets is too HIGH for effective attack on 1-core CPU - attack will be started with ONE copy (but multiply targets)\033[0;0m\n"
 		copies_num_sametargets=2
 	fi
-	if ((num_of_copies > 2));
+	if ((num_of_copies > 6));
 	then
 		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$num_of_copies is too HIGH for effective attack on 1-core CPU - attack will be started with ONE copy (but multiply targets)\033[0;0m\n"
-		num_of_copies=1
+		num_of_copies=2
 	fi
 fi
 sleep 5s
